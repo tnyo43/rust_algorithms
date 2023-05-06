3363a662a6b51eefba5014187a8401ef511db096
Tomoya Kashifuku <tnyo43@gmail.com>
2023-05-06 22:53:49 +0900

    add vector2

---
+++ packages/algorithms/src/geometry/vector.rs
@@ -0,0 +1,70 @@
+use std::ops;
+
+#[derive(Clone, Copy)]
+pub struct Vector2<Data>(Data, Data);
+
+impl<Data> Vector2<Data> {
+    pub fn new(x: Data, y: Data) -> Self {
+        Self(x, y)
+    }
+}
+
+impl<Data> ops::Add for Vector2<Data>
+where
+    Data: ops::Add<Output = Data>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl<Data> ops::AddAssign for Vector2<Data>
+where
+    Data: ops::Add<Output = Data> + Copy,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs
+    }
+}
+
+impl<Data> ops::Sub for Vector2<Data>
+where
+    Data: ops::Sub<Output = Data>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl<Data> ops::SubAssign for Vector2<Data>
+where
+    Data: ops::Sub<Output = Data> + Copy,
+{
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs
+    }
+}
+
+impl<Data> ops::Mul<Data> for Vector2<Data>
+where
+    Data: ops::Mul<Output = Data> + Copy,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Data) -> Self::Output {
+        Self::new(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+impl<Data> ops::MulAssign<Data> for Vector2<Data>
+where
+    Data: ops::Mul<Output = Data> + Copy,
+{
+    fn mul_assign(&mut self, rhs: Data) {
+        *self = *self * rhs
+    }
+}
--- packages/algorithms/src/lib.rs
+++ packages/algorithms/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod data_structure;
+pub mod geometry;
 pub mod graph;
 pub mod math;
