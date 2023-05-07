08ec7637f392e241d9046f659a3b23f84df6e642
Tomoya Kashifuku <tnyo43@gmail.com>
2023-05-07 14:30:50 +0900

    add num-traits

--- packages/algorithms/src/geometry/point2.rs
+++ packages/algorithms/src/geometry/point2.rs
@@ -1,18 +1,17 @@
 use std::ops;
 
-#[derive(Clone, Copy)]
-pub struct Point2<Data>(Data, Data);
+use num_traits::Num;
 
-impl<Data> Point2<Data> {
+#[derive(Clone, Copy, PartialEq)]
+pub struct Point2<Data: Num>(pub Data, pub Data);
+
+impl<Data: Num> Point2<Data> {
     pub fn new(x: Data, y: Data) -> Self {
         Self(x, y)
     }
 }
 
-impl<Data> ops::Add for Point2<Data>
-where
-    Data: ops::Add<Output = Data>,
-{
+impl<Data: Num> ops::Add for Point2<Data> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -20,19 +19,13 @@ where
     }
 }
 
-impl<Data> ops::AddAssign for Point2<Data>
-where
-    Data: ops::Add<Output = Data> + Copy,
-{
+impl<Data: Num + Copy> ops::AddAssign for Point2<Data> {
     fn add_assign(&mut self, rhs: Self) {
         *self = *self + rhs
     }
 }
 
-impl<Data> ops::Sub for Point2<Data>
-where
-    Data: ops::Sub<Output = Data>,
-{
+impl<Data: Num> ops::Sub for Point2<Data> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -40,19 +33,13 @@ where
     }
 }
 
-impl<Data> ops::SubAssign for Point2<Data>
-where
-    Data: ops::Sub<Output = Data> + Copy,
-{
+impl<Data: Num + Copy> ops::SubAssign for Point2<Data> {
     fn sub_assign(&mut self, rhs: Self) {
         *self = *self - rhs
     }
 }
 
-impl<Data> ops::Mul<Data> for Point2<Data>
-where
-    Data: ops::Mul<Output = Data> + Copy,
-{
+impl<Data: Num + Copy> ops::Mul<Data> for Point2<Data> {
     type Output = Self;
 
     fn mul(self, rhs: Data) -> Self::Output {
@@ -60,19 +47,13 @@ where
     }
 }
 
-impl<Data> ops::MulAssign<Data> for Point2<Data>
-where
-    Data: ops::Mul<Output = Data> + Copy,
-{
+impl<Data: Num + Copy> ops::MulAssign<Data> for Point2<Data> {
     fn mul_assign(&mut self, rhs: Data) {
         *self = *self * rhs
     }
 }
 
-impl<Data> Point2<Data>
-where
-    Data: ops::Sub<Output = Data> + ops::Mul<Output = Data>,
-{
+impl<Data: Num> Point2<Data> {
     pub fn det(self, rhs: Self) -> Data {
         self.0 * rhs.1 - self.1 * rhs.0
     }
